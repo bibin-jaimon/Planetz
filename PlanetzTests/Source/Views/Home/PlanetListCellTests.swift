2eb78eedb507282df98f5ebc7ab34be1de74aed6
bibin-jaimon <bibinjaimon@gmail.com>
2023-05-08 23:09:04 +0530

    Add unit test for PlanetListCell

---
+++ PlanetzTests/Source/Views/Home/PlanetListCellTests.swift
@@ -0,0 +1,32 @@
+//
+//  PlanetListCellTests.swift
+//  PlanetzTests
+//
+//  Created by Bibin Jaimon on 08/05/23.
+//
+
+import XCTest
+@testable import Planetz
+
+final class PlanetListCellTests: XCTestCase {
+    
+    func testPlanetListCell_reuseIdentifier() {
+        XCTAssertEqual(PlanetListCell.identifier, "PlanetListCell")
+    }
+
+    func testExample() throws {
+        
+        let sut = PlanetListCell()
+        let planet = MockPlanetResponse.planets.first!
+        sut.setupCell(planet: planet)
+        
+        XCTAssertNotNil(sut.contentConfiguration)
+        
+        let config = sut.contentConfiguration as? UIListContentConfiguration
+        
+        XCTAssertEqual(config?.text, planet.name)
+        XCTAssertEqual(config?.secondaryText, planet.terrain)
+        XCTAssertNotNil(config?.image)
+    }
+
+}
